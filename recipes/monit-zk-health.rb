065fba0271016a525c7932190b3ae4c2e3b0360d
Daland Montgomery <davidmontgomery@gmail.com>
2015-09-20 15:47:19 +0800

    ..

---
+++ recipes/monit-zk-health.rb
@@ -0,0 +1,9 @@
+service "monit"
+template "/etc/monit/conf.d/.zookeeper.health.conf" do
+  path "/etc/monit/conf.d/.zookeeper.health.conf"
+  source "monit.zookeeper.health.conf.erb"
+  owner "root"
+  group "root"
+  mode "0755"
+  notifies :restart, resources(:service => "monit")
+end
