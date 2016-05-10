46a952748e38a6e1c15b3d1d90f7b3830cb371c9
David Montgomery <davidmontgomery@gmail.com>
2016-05-10 15:10:30 +0800

    ua

--- recipes/zookeeper_service.rb
+++ recipes/zookeeper_service.rb
@@ -53,11 +53,23 @@ zk_process_monitor_list = node['zk_process_monitor']
 #  f.write(zk_hosts.to_json)
 #end
 
+
+
+
+
 easy_install_package "zc.zk" do
   action :install
 end
 
 easy_install_package "psutil" do
+  action :install
+end
+
+package "libffi-dev" do
+  action :install
+end
+
+package "libssl-dev" do
   action :install
 end
 
